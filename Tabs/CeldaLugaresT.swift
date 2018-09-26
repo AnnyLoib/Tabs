13b673b041e5caa2e13d128619a7fab1c7738032
Alumno <ulsa@iMac-de-Alumno.local>
2018-09-25 18:57:07 -0700

    Tabs en Navegation

---
+++ Tabs/CeldaLugaresT.swift
@@ -0,0 +1,14 @@
+//
+//  CeldaLugaresT.swift
+//  Tabs
+//
+//  Created by Alumno on 25/09/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaLugaresT: UITableViewCell {
+    
+}
